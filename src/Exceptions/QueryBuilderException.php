4253e5f001a03a3fdeabe4c10b54ba7a82e89eba
Craigus Waigus <craig@craigify.com>
2017-09-19 16:14:55 -0500

    Fixed the rest of the Indicium exception classes with the same constructor namespace typo

--- src/Exceptions/QueryBuilderException.php
+++ src/Exceptions/QueryBuilderException.php
@@ -13,7 +13,7 @@ class QueryBuilderException extends IndiciumException
 	protected $query = "";
 
 	// Constructor
-	public function __construct($message, $code=0, Exception $previous = null)
+	public function __construct($message, $code=0, \Exception $previous = null)
 	{
 		parent::__construct($message, $code, $previous);
 	}
